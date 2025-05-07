3a573f9906fd46521d2a8831c2e96bc647bf55d3
allyradomski <100952670+allyradomski@users.noreply.github.com>
2025-05-07 15:55:47 +0100

    test

--- assets/posts/2025/an-array-of-floating-ears/sketch.js
+++ assets/posts/2025/an-array-of-floating-ears/sketch.js
@@ -20,11 +20,11 @@ function windowResized() {
     resizeCanvas(container.clientWidth, container.clientHeight);
 }
 
-window.onload = function() {
-    if (parent) {
-        var oHead = document.getElementsByTagName("head")[0];
-        var arrStyleSheets = parent.document.getElementsByTagName("style");
-        for (var i = 0; i < arrStyleSheets.length; i++)
-            oHead.appendChild(arrStyleSheets[i].cloneNode(true));
-    }
-}
+// window.onload = function() {
+//     if (parent) {
+//         var oHead = document.getElementsByTagName("head")[0];
+//         var arrStyleSheets = parent.document.getElementsByTagName("style");
+//         for (var i = 0; i < arrStyleSheets.length; i++)
+//             oHead.appendChild(arrStyleSheets[i].cloneNode(true));
+//     }
+// }
