28b7b9d829203dee8bf2748c554cea3f98504a33
allyradomski <100952670+allyradomski@users.noreply.github.com>
2025-05-07 16:33:31 +0100

    huhu

--- assets/posts/2025/an-array-of-floating-ears/sketch.js
+++ assets/posts/2025/an-array-of-floating-ears/sketch.js
@@ -1,7 +1,13 @@
+const numCols = 50;
+let numRows;
+
 let container = document.getElementById("processing");
 function setup() {
 
     let cnv = createCanvas(container.offsetWidth, container.offsetHeight);
+
+    numRows = (container.offsetHeight / container.offsetWidth) * numCols;
+
     cnv.parent("processing");
 
 }
@@ -9,12 +15,29 @@ function setup() {
 
 function draw() {
     background(34, 34, 34);
-    fill(255);
-    stroke(255);
-    textSize(64);
-    text("ear", random(width), random(height));
-}
+    // fill(255);
+    // stroke(255);
+    // textSize(64);
+    // text("ear", random(width), random(height));
 
+    fill(255);
+    noStroke();
+    rectMode(CENTER);
+    let maxWidth = width / numCols;
+    for (let i = 0; i < numCols; i++) {
+        for (let j = 0; j < numRows; j++) {
+            let val = noise(
+                map(i, 0, numCols, 0, 1) * 3,
+                map(j, 0, numRows, 0, 1) * 3,
+                millis() * 0.0005,
+            )
+
+            
+            square(map(i + 0.5, 0, numCols, 0, width), map(j + 0.5, 0, numRows, 0, height), maxWidth * val);
+            
+        }
+    }
+}
 
 function windowResized() {
     resizeCanvas(container.clientWidth, container.clientHeight);
