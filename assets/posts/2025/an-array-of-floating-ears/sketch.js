0111235f94eb9b7158da05798a8615cf0ed45ff6
allyradomski <100952670+allyradomski@users.noreply.github.com>
2025-05-07 15:49:29 +0100

    test

--- assets/posts/2025/an-array-of-floating-ears/sketch.js
+++ assets/posts/2025/an-array-of-floating-ears/sketch.js
@@ -1,11 +1,30 @@
+let container = document.getElementById("processing");
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+
+    let cnv = createCanvas(container.offsetWidth, container.offsetHeight);
+    cnv.parent("processing");
+
 }
 
+
 function draw() {
-    background(34, 34, 34, 30);
+    background(34, 34, 34);
     fill(255);
     stroke(255);
     textSize(64);
-    text('ear', random(width), random(height));
+    text("ear", random(width), random(height));
+}
+
+
+function windowResized() {
+    resizeCanvas(container.clientWidth, container.clientHeight);
+}
+
+window.onload = function() {
+    if (parent) {
+        var oHead = document.getElementsByTagName("head")[0];
+        var arrStyleSheets = parent.document.getElementsByTagName("style");
+        for (var i = 0; i < arrStyleSheets.length; i++)
+            oHead.appendChild(arrStyleSheets[i].cloneNode(true));
+    }
 }
